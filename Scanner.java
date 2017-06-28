1f9d6297045936cf19593b1632fa91a69f5d0855
nikito7ik <nikitakovalev1999@gmail.com>
2017-06-28 18:25:03 +0300

    Add files via upload

---
+++ Scanner.java
@@ -0,0 +1,13 @@
+package com.nikita.kovalev;
+
+/**
+ * Created by nikit on 28.06.2017.
+ * Scanner
+ */
+public class Scanner {
+
+    public static int Scan() {
+        java.util.Scanner scanner = new java.util.Scanner(System.in);
+        return scanner.nextInt();
+    }
+}
